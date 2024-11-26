57aa85d0321449810acee54c7daa4e83aede42bb
Charitarth Chugh <37895518+charitarthchugh@users.noreply.github.com>
2024-11-26 11:58:32 -0500

    refactor(data): refactor dataset and start quashing bugs
    - Move filtering logic outside of dataset so it only has to be done once across each split.
    - Add transforms logic.
    - Quash bugs around creating masks and create more unified logic detections are first converted to polygons then to masks.
    - Test the ability to instantiate with hydra.
    
     TODO: Move away from fiftyone. Fiftyone is not fork safe so it cannot be used in a pytorch dataloader.
    
    BREAKING CHANGE: there is now no way to filter a dataset within the dataset. all filtered fiftyone views must be passed in to the dataset

--- src/lightningsparseinst/data/dataset.py
+++ src/lightningsparseinst/data/dataset.py
@@ -1,4 +1,3 @@
-import re
 from typing import Literal, Optional
 
 import albumentations as A
@@ -7,16 +6,17 @@ import hydra
 import numpy as np
 import omegaconf
 import torch
-from PIL import Image
 from albumentations.pytorch import ToTensorV2
-from fiftyone import ViewField
-from nn_core.common import PROJECT_ROOT
+from PIL import Image
 from torch.utils.data import Dataset
+
+from nn_core.common import PROJECT_ROOT
+
+from lightningsparseinst.utils.transforms import ResizeShortestEdge
 
 
 class SegmentationDataset(Dataset):
-    """
-    A custom dataset class for segmentation tasks using the FiftyOne library.
+    """A custom dataset class for segmentation tasks using the FiftyOne library.
 
     Args:
         fiftyone_dataset (fo.Dataset | str): The FiftyOne dataset or name of the dataset to load.
@@ -38,86 +38,83 @@ class SegmentationDataset(Dataset):
         labels_map_rev (dict): A dictionary mapping class labels to their indices.
         transform (A.Compose): The transformation to apply to dataset items.
     """
-    def __init__(self, fiftyone_dataset: fo.Dataset | str, split: Optional[str] = None,
-                 classes: Optional[list[str] | str] = None,
-                 gt_field="ground_truth",
-                 detection_field: Optional[Literal['detections', 'polylines', 'segmentations']] = 'detections',
-                 transform: Optional[A.Compose] = None
-                 ):
 
-        if isinstance(fiftyone_dataset, str):
-            s = fo.load_dataset(fiftyone_dataset)
+    def __init__(
+        self,
+        ref: fo.Dataset | str,
+        name: Optional[str] = None,
+        split: str = "train",
+        gt_field: Optional[str] = "ground_truth",
+        detection_field: Optional[Literal["detections", "polylines", "segmentations"]] = "detections",
+        max_num_instances_per_image: Optional[int] = None,
+        transform: Optional[A.Compose] = None,
+    ):
+        if isinstance(ref, str):
+            s = fo.load_dataset(ref)
         else:
-            s = fiftyone_dataset
+            s = ref
+        self.name = name
 
-        if len(s.tags) <= 1:
-            self.samples = s
-        elif split is not None:
-            self.samples = s.match_tags(split)
-        else:
-            raise ValueError("Either split or tags must be specified.")
+        self.samples = s.match_tags(split)
 
         self.gt_field = gt_field
         # Ensure that if the detection field is detections, it has a mask field
         self.detection_field = detection_field
         if self.detection_field == "detections" and not self.samples.has_field(f"{self.gt_field}.detections.mask"):
             raise ValueError("Detections field must have a mask field, need masks to perform segmentation")
-        elif self.detection_field == 'segmentations' and not self.samples.has_field(
-            f"{self.gt_field}.segmentations.labels"):
-            raise ValueError("Segmentations field must have a labels field, need labels to perform segmentation")
         # Getting classes
-        self.classes = classes
-        if not self.classes:
-            # Get list of distinct labels that exist in the view
-            self.classes = self.samples.distinct(f"{self.gt_field}.{self.detection_field}.label")
-        # needs to be tested... implemented because of the our of roi class in cityscapes
-        elif isinstance(self.classes, list):
-            self.samples = set(self.samples.filter_labels(f"{self.gt_field}.{self.detection_field}",
-                                                          ViewField('label').is_in(self.classes)))
-        elif isinstance(self.classes, str):
-            regex = re.compile(self.classes)
-            self.classes = list(
-                filter(regex.search, self.samples.distinct(f"{self.gt_field}.{self.detection_field}.label")))
-            self.samples = self.samples.filter_labels(f"{self.gt_field}.{self.detection_field}",
-                                                      ViewField('label').is_in(self.classes))
+        self.classes: list = list(self.samples.distinct(f"{self.gt_field}.{self.detection_field}.label"))
         self.img_paths = self.samples.values("filepath")
         self.labels_map_rev = {cls: idx for idx, cls in enumerate(self.classes)}
 
-        if transform:
-            self.transform = transform
-        else:
-            self.transform = A.Compose([ToTensorV2()])
+        self.max_num_instances_per_image = max_num_instances_per_image
+        if not self.max_num_instances_per_image:
+            self.max_num_instances_per_image = len(self.classes)
 
-    def __getitem__(self, idx):
+        if split == "train" and transform is not None:
+            self.transform: A.Compose = transform
+        else:
+            self.transform: A.Compose = A.Compose([A.Compose([ResizeShortestEdge(640, 853)]), ToTensorV2()])
+
+    def __getitem__(self, idx: int):
         img_path = self.img_paths[idx]
         sample = self.samples[img_path]
         img = Image.open(img_path).convert("RGB")
+        img_size = img.size
+        img = np.asarray(img)
+
+        if not sample[self.gt_field]:
+            transformed = self.transform(image=np.asarray(img))
+
+            return transformed["image"], torch.tensor([]), torch.tensor([])
+
+        ground_truth = sample[self.gt_field]
+        if self.detection_field == "detections":
+            ground_truth = ground_truth.to_polylines()
 
         masks = []
         labels = []
-        if self.detection_field == "polylines":
-            for polyline in sample[self.gt_field].polylines:
-                # The output of a segmentation conversion here is 0 for false and 255 for true. We want it to be 1 for true
-                arr = polyline.to_segmentation(frame_size=img.size).mask
-                # when the arr is 255, it is true, else false, so this becomes a bool array that we cast into uint8
-                arr = (arr == 255).astype(
-                    np.uint8)  # Theoretically, we can just divide the array by 255 but this should be faster.
-                masks.append(arr)
-                labels.append(self.labels_map_rev[polyline.label])
+        for polyline in ground_truth.polylines:
+            # The output of a segmentation conversion here is 0 for false and 255 for true. We want it to be 1 for true
+            arr = polyline.to_segmentation(frame_size=img_size, target=1).mask
+            masks.append(arr)
+            labels.append(self.labels_map_rev[polyline.label])
+        if len(masks) < len(self.classes):
+            masks += [np.zeros(img_size, dtype=np.uint8)] * (len(self.classes) - len(masks))
+            labels += [self.labels_map_rev[polyline.label]] * (len(self.classes) - len(masks))
+        #
+        # else:
+        #     # we assume that mask property is present for detections
+        #     # masks = [detection.mask.astype(np.uint8) for detection in sample[self.gt_field].detections]
+        #     # labels = [self.labels_map_rev[detection.label] for detection in sample[self.gt_field].detections]
+        #     for detection in sample[self.gt_field].detections:
+        #         arr = detection.to_segmentation(frame_size=img_size).mask
+        #         arr = (arr == 255).astype(np.uint8)
+        #         masks.append(arr)
+        #         labels.append(self.labels_map_rev[detection.label])
 
-        else:
-            # we assume that mask property is present for detections
-            # masks = [detection.mask.astype(np.uint8) for detection in sample[self.gt_field].detections]
-            # labels = [self.labels_map_rev[detection.label] for detection in sample[self.gt_field].detections]
-            for detection in sample[self.gt_field].detections:
-                arr = detection.to_segmentation(frame_size=img.size).mask
-                arr = (arr == 255).astype(np.uint8)
-                masks.append(arr)
-                labels.append(self.labels_map_rev[detection.label])
-
-        transformed = self.transform(image=np.asarray(img), masks=masks)
-        return transformed['image'], torch.stack(transformed['masks']), torch.tensor(labels)
-
+        transformed = self.transform(image=img, masks=masks)
+        return (transformed["image"], torch.stack(transformed["masks"]), torch.tensor(labels))
 
     def __len__(self):
         return len(self.img_paths)
@@ -126,14 +123,16 @@ class SegmentationDataset(Dataset):
         return self.classes[id]
 
 
-@hydra.main(config_path=str(PROJECT_ROOT / "conf"), config_name="default")
+@hydra.main(config_path=str(PROJECT_ROOT / "conf"), config_name="default", version_base="1.1")
 def main(cfg: omegaconf.DictConfig) -> None:
     """Debug main to quickly develop the Dataset.
 
     Args:
         cfg: the hydra configuration
     """
-    _: Dataset = hydra.utils.instantiate(cfg.nn.data.dataset, _recursive_=False)
+    ds: SegmentationDataset = hydra.utils.instantiate(cfg.nn.data.dataset)
+
+    var = ds[0]  # noqa
 
 
 if __name__ == "__main__":
